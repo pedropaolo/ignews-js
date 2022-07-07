84c0220b57c3bd917d446a31d3ca157c68d008b4
Dev Paolo <paolo.pedro.picinin@gmail.com>
2022-07-06 21:34:52 -0300

    Estilização Home - préLogin

--- ignews/src/pages/_app.tsx
+++ ignews/src/pages/_app.tsx
@@ -2,7 +2,7 @@
 import { AppProps } from 'next/app'
 // Para funcionar foi adicionado: declare module '*.scss'; no arquivo next-env.d.ts
 import '../../styles/global.scss';
-import { Header } from './components/Header';
+import { Header } from '../components/Header';
 
 function MyApp({ Component, pageProps } : AppProps) {
   return (
--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,10 +1,42 @@
-
+import Head from 'next/head'
+import styles from './home.module.scss'
 
 
 export default function Home() {
   return (
     <>
-      <h1>Paolinho o dev fenômeno!</h1>
+     <Head>
+      <title>Home | ig.nes</title>
+     </Head>
+
+     <main className={styles.contentContainer}>
+
+      {/* Conteúdo em texto apresentado antes do login */}
+
+      <section className={styles.hero}>
+
+        <span>Seja Bem-Vindo!</span>
+
+        <h1>
+          Novidades sobre o mundo   do <span>React</span>
+        </h1>
+
+        <p>
+          Garanta acesso às todas as publicações por <span>R$ 9.99/mês</span>
+        </p>
+
+
+      </section>
+
+      {/* SVG apresentado antes do login */}
+
+      <img src="/images/avatar.svg" alt="Garota programando" />
+
+
+
+     </main>
+
+
     </>
   )
 }
